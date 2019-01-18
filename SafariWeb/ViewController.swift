167917407a9ce24bf92307c8f79856db500b8809
Kelvin Tan <kelvin.vins@gmail.com>
2019-01-18 14:35:32 +0800

    Initial Commit

---
+++ SafariWeb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafariWeb
+//
+//  Created by Kelvin Tan on 1/18/19.
+//  Copyright © 2019 Kelvin Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
